c73b04792813931f7f2f909254d57462739face1
root <moustafayoussef759@gmail.com>
2020-04-20 15:38:08 +0200

    editing for deploying in sec-project

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <html>
  <head>
   <title>PHP Test</title>
+  <title>DEPLOYING THE s2i APPLICATION IN THE SECOND-PROJECT <title>
  </head>
 
  <body>
-  <?php echo '<p>This is moustafa trying to edit the following sentence/p>' ; ?>
+  <?php echo '<p>This is moustafa trying to edit the following sentence/ <p>' ; ?>
  </body>
 
 </html>
