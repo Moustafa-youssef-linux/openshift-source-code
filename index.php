5ff34f2faafce6ee986d3e8d7e5e13ece1819db0
root <moustafayoussef759@gmail.com>
2020-04-20 15:48:18 +0200

    Updating

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <html>
  <head>
   <title>PHP Test</title>
-  <title>DEPLOYING THE s2i APPLICATION IN THE SECOND-PROJECT <title>
  </head>
 
  <body>
-  <?php echo '<p>This is moustafa trying to edit the following sentence/ <p>' ; ?>
+  <?php echo '<p>DEPLOYING THE s2i APPLICATION IN THE SECOND-PROJECT <p>' ; ?>
  </body>
 
 </html>
